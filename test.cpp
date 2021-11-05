34cea02fb7fdc0c3213824f65fdfe9690e71cc50
letranger <yen.yungchin@gmail.com>
2021-11-05 13:43:41 +0800

    Update

--- test.cpp
+++ test.cpp
@@ -1,26 +1,10 @@
-#include <algorithm>
-#include <cmath>
-#include <cstdio>
-#include <cstdlib>
 #include <iostream>
 using namespace std;
 int main() {
-    int test;
-    test = 29;
-    test = test + 10;
-    cout << test;
+    int test = 3;
 
-    for (int i = 0; i < test; i++) {
-        cout << "TEST";
-        if (i % 2 == 0) { //偶數
-            printf("%d\n", i);
-        } else {
-            printf("%d\n", 2*i);
+    for (int i = 1; i <=100; i++) {
+        cout << i + test << endl;
         }
-    }
-
 }
 
-/*
-This is a test
-*/
